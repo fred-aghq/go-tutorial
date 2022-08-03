c64d725998db6352945e3298ba9afeb70f8d0b44
Fred Spooner <fred@Freds-MacBook-Pro.local>
2022-08-03 19:07:22 +0100

    validate inputs

--- main.go
+++ main.go
@@ -23,9 +23,12 @@ func bookTickets() bool {
 	var firstName string
 	var lastName string
 	var email string
+	var userTickets uint
 
 	fmt.Println("Please enter your first name:")
 	fmt.Scan(&firstName)
+
+	// isValidName := validateName(firstName, lastName)
 
 	fmt.Println("Please enter your last name:")
 	fmt.Scan(&lastName)
@@ -35,37 +38,44 @@ func bookTickets() bool {
 
 	fmt.Printf("Thanks %s. How many tickets would you like?:\n", firstName)
 
-	var userTickets uint
 	fmt.Scan(&userTickets)
+
+	for userTickets > remainingTickets {
+		fmt.Println("That exceeds the number of remaining tickets.")
+		fmt.Printf("Please enter a valid amount. There are %d tickets remaining:\n", remainingTickets)
+		fmt.Scan(&userTickets)
+	}
 
 	return processBooking(userTickets, firstName, lastName, email)
 }
 
 func processBooking(userTickets uint, firstName string, lastName string, email string) bool {
-	if userTickets < remainingTickets {
+	if validateName(firstName, lastName) && validateEmail(email) {
 		remainingTickets = remainingTickets - userTickets
 		bookings = append(bookings, firstName+" "+lastName)
 		fmt.Printf("User %s booked %d tickets.\n", email, userTickets)
 		return true
 	}
 
-	fmt.Println("That exceeds the number of remaining tickets")
-
 	return false
+}
+
+func validateName(firstName string, lastName string) bool {
+	return len(firstName) >= 2 && len(lastName) >= 2
+}
+
+func validateEmail(email string) bool {
+	return strings.Contains(email, "@")
 }
 
 func main() {
 	firstNames := []string{}
 
-	for {
-		if remainingTickets == 0 {
-			fmt.Println("Unfortunately there are no tickets remaining.")
-			break
-		}
-
+	for remainingTickets > 0 {
 		printHello()
 
 		if !bookTickets() {
+			fmt.Println("Sorry, your information was invalid. Please try again.")
 			continue
 		}
 
@@ -79,4 +89,6 @@ func main() {
 		fmt.Println()
 		fmt.Printf("All bookings: \n\n%v\n\n", firstNames)
 	}
+
+	fmt.Println("Unfortunately there are no tickets remaining.")
 }
